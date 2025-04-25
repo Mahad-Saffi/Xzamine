6dd50bce7dbb6e4e7a927bf131c65c262ace8e67
Mahad Saffi <mahadsaffi@gmail.com>
2025-04-25 23:42:58 +0500

    message

--- server/server.py
+++ server/server.py
@@ -18,17 +18,15 @@ def home():
 def get_data():
     data = request.json
     post = data.get("postTextBody")
-    print("Received post data:", data)
     
-    # result = invoke_model(post)
     result = invoke_model(post)
     
-    # highest_label, highest_value = max(result['probabilities'].items(), key=lambda x: x[1])
     highest_label, highest_value = max(result['probabilities'].items(), key=lambda x: x[1])
 
     print("Highest probability class:", highest_label)
     print("Probability:", highest_value)
     print("Result:", result)
+    
 
     # from here call the model and get the result for now i have set it to the false making each post being analyzed illegal
     return jsonify({"sentiment": highest_label, "received": data})
